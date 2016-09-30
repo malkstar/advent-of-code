0a77de8d64fc8af366cf1aeb86365c59c7629439
Jim Malkin <jim@bullionbypost.co.uk>
2016-09-30 17:17:37 +0100

    up to day 5

---
+++ day4/part2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import hashlib
+
+with open('input.txt', 'r') as input_file:
+    input_text = input_file.read().strip()
+    m = hashlib.md5(input_text)
+    appended_number = 0
+    while m.hexdigest()[0:6] != '000000':
+        m = hashlib.md5(input_text)
+        m.update(str(appended_number))
+        appended_number += 1
+    print('ANSWER: {}'.format(appended_number - 1))
---
+++ day5/part1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+
+def has_double_letters(string):
+    last_char = ''
+    for char in string:
+        if char == last_char:
+            return True
+        last_char = char
+    return False
+
+
+def has_three_vowels(string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    used_vowels = 0
+    for vowel in vowels:
+        if vowel in string:
+            used_vowels += 1
+            if used_vowels > 2:
+                return True
+    return False
+
+
+def has_consecutive_letters(string):
+    bad_strings = ['ab', 'cd', 'pq', 'xy']
+    for bad_string in bad_strings:
+        if bad_string in string:
+            return True
+    return False
+
+
+def is_nice(string):
+    print(string)
+    print(has_double_letters(string))
+    print(has_three_vowels(string))
+    print(has_consecutive_letters(string))
+    return has_double_letters(string) and has_three_vowels(string) and not has_consecutive_letters(string)
+
+
+NICE_STRINGS = 0
+
+with open('input.txt', 'r') as input_file:
+    for line in input_file:
+        if is_nice(line.strip()):
+            NICE_STRINGS += 1
+    print(NICE_STRINGS)
