2a784b97c73f3eeea1c36c3e89fc7f19484a911c
Jim <jamesm@bullionbypost.co.uk>
2018-12-14 15:43:17 +0000

    Edit day1 to use sets as we've found this is the make or break for getting result in under 3 mins.

--- 2018/day1/part2.py
+++ 2018/day1/part2.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 with open('input.txt', 'r') as input_file:
     input_text_lines = input_file.readlines()
 
-    results = [0,]
+    results = set([0,])
     current_frequency = 0
 
     def until_result_repeated(text_lines):
@@ -14,13 +14,13 @@ with open('input.txt', 'r') as input_file:
         found_result = False
         for line in text_lines:
             new_value = current_frequency + int(line)
-            if new_value in results:
+            current_frequency = new_value
+            result_count = len(results)
+            results.add(current_frequency)
+            if len(results) == result_count:
                 print('First repeated: {}'.format(new_value))
                 found_result = True
                 break
-            else:
-                current_frequency = new_value
-                results.append(current_frequency)
 
         if not found_result:
             until_result_repeated(text_lines)
